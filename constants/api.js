516f577ddcd150d193ce19f295ce7f2f78aae899
Tima <tima@inkubasia.com>
2026-06-10 14:18:37 +0600

    Switch API URL to production

--- constants/api.js
+++ constants/api.js
@@ -1,5 +1,5 @@
-export const API_URL = "http://192.168.1.5:5001/api";
-//export const API_URL = "https://shortcut-wallet.onrender.com/api";
+//export const API_URL = "http://192.168.1.5:5001/api";
+export const API_URL = "https://shortcut-wallet.onrender.com/api";
 export const ACCOUNTS_URL = `${API_URL}/accounts`;
 export const NOTES_URL = `${API_URL}/notes`;
 export const PDF_URL = `${API_URL}/pdf`;
